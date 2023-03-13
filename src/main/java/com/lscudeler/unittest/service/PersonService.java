af9676c2302cdd66ed52da0391d9c1bd8de58197
Luis Scudeler <luisscudeler@yahoo.com.br>
2023-03-13 10:26:45 -0300

    Implementing Unit test part - 2 (testing if exception is that we choose - in this exemple NO)

--- src/main/java/com/lscudeler/unittest/service/PersonService.java
+++ src/main/java/com/lscudeler/unittest/service/PersonService.java
@@ -5,8 +5,11 @@ import com.lscudeler.unittest.dominio.Person;
 import java.util.Objects;
 
 public class PersonService {
-    public boolean isAdult(Person person){
-        Objects.requireNonNull(person, "Person can´t be null");
+    public boolean isAdult(Person person) {
+        //Objects.requireNonNull(person, "Person can´t be null");
+        if(person == null){
+            throw new IllegalArgumentException();
+        }
         return person.getAge() >= 18;
     }
 }
--- src/test/java/com/lscudeler/unittest/service/PersonServiceTest.java
+++ src/test/java/com/lscudeler/unittest/service/PersonServiceTest.java
@@ -2,19 +2,39 @@ package com.lscudeler.unittest.service;
 
 import com.lscudeler.unittest.dominio.Person;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonServiceTest {
+    private Person adult;
+    private Person notAdult;
+    private PersonService personService;
+
+    @BeforeEach  //com essa notação essa classe será executada antes de cada um dos testes unitários
+    public void setUp(){
+        adult = new Person(18);
+        notAdult = new Person(15);
+        personService = new PersonService();
+    }
 
     @Test
     @DisplayName("A person should be not adult when age is lower than 18 ")
     void isAdult_ReturnFalse_WhenAgeIsLowerThan18() {
+        Assertions.assertFalse(personService.isAdult(notAdult));
+    }
 
-        Person person = new Person(15);
-        PersonService personService = new PersonService();
-        Assertions.assertFalse(personService.isAdult(person));
+    @Test
+    @DisplayName("A person should be adult when age is greater or equal than 18 ")
+    void isAdult_ReturnTrue_WhenAgeIsGreaterOrEqualsThan18() {
+        Assertions.assertTrue(personService.isAdult(adult)); //Verifica se o resultado retorna true
+    }
+
+    @Test
+    @DisplayName("Should throw NullPointerException with message when person is null ")
+    void isAdult_ShouldThrowException_WhenPersonIsNull() {
+        Assertions.assertThrows(NullPointerException.class, () -> personService.isAdult(null));
     }
 }
